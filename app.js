efedea052a6636ac6753a73feaf7136deb950e9f
Kaio991 <kaioalencar.santos@gmail.com>
2026-04-23 21:16:20 -0300

    feat: Deploy com fullstack

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const app = express();
 
 app.use(cors())
 app.use(express.json())
-app.use(express.static(path.resolve(__dirname,"public")))
+app.use(express.static(path.join(process.cwd(),"public")))
 conectarBanco()
 app.use(routerProdutos)
 app.use(routerUsuario)
@@ -25,7 +25,7 @@ app.use(routerCarrinho)
 app.use(routerPedido)
 
 app.get("/",(req,res)=>{
-    res.sendFile(path.resolve(__dirname,"public","index.html"))
+    res.sendFile(path.join(process.cwd(),"public","pages","index.html"))
 })
 
 app.use(erros)
