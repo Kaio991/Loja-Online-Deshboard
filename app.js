6a0957dd32623d8348c02d5c9002a5ff0a8695db
Kaio991 <kaioalencar.santos@gmail.com>
2026-04-23 21:00:34 -0300

    feat: deploy com o front-end integrado

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 import "dotenv/config";
 import express from "express";
 import cors from "cors";
+import path from "path"
+import { fileURLToPath } from "url";
 import {conectarBanco} from "./src/db/db.js";
 import routerProdutos from "./src/routes/produtoRoutes.js";
 import routerUsuario from "./src/routes/usuariosRoutes.js";
@@ -8,18 +10,26 @@ import routerCarrinho from "./src/routes/carrinhoRoutes.js";
 import { erros } from "./src/middlewares/erros.js";
 import routerPedido from "./src/routes/pedidosRoutes.js";
 
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const app = express();
 
 app.use(cors())
 app.use(express.json())
-app.use(express.static('public'))
+app.use(express.static(path.join(__dirname,"public")))
 conectarBanco()
 app.use(routerProdutos)
 app.use(routerUsuario)
 app.use(routerCarrinho)
 app.use(routerPedido)
 
+app.get("/",(req,res)=>{
+    res.sendFile(path.join(__dirname,"public","index.html"))
+})
+
 app.use(erros)
+
 
 const port = process.env.PORT
 app.listen(port,"0.0.0.0",()=>{
